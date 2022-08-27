df092cc4ceefeefc8978364ed59d4709e5c040eb
Gedr <gimaev.ufa@gmail.com>
2022-08-27 17:13:23 +0900

    translate a visit page into english

--- app.rb
+++ app.rb
@@ -7,7 +7,9 @@ get '/' do
 	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
 end
 get '/about' do
-	"Hello! This page displayes all information about our team"
+	erb :about
   end
 
-  
+  get '/visit' do
+	erb :visit
+  end  
