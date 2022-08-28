ba7dd46ce0c47bc59679207d43b9faf2178a2e7a
Gedr <gimaev.ufa@gmail.com>
2022-08-28 15:06:52 +0900

    Insert data of clients to DB

--- app.rb
+++ app.rb
@@ -5,8 +5,32 @@ require 'sinatra/reloader'
 require 'sqlite3'
 
 configure do 
-	@db = SQLite3::Database.new 'barbershop.db'
-	
+	get_db = SQLite3::Database.new 'barbershop.db'
+	db = get_db
+	db.execute 'CREATE TABLE IF NOT EXISTS 
+    "Clients" 
+    (
+        "id" INTEGER PRIMARY KEY AUTOINCREMENT,
+        "username" text(128),
+        "phone" text(128),
+        "datestamp" text(128),
+        "barber" text(128),
+        "color" text(128)
+    )'
+
+	db.execute 'CREATE TABLE IF NOT EXISTS 
+		"Barbers" 
+		(
+			"ID" integer PRIMARY KEY AUTOINCREMENT NOT NULL,
+			"barbername" text(128)
+		)'
+	db.execute 'CREATE TABLE IF NOT EXISTS 
+		"Contacts" 
+		(
+			"ID" integer PRIMARY KEY AUTOINCREMENT NOT NULL,
+			"username" text(128),
+			"usermessage" text(128)
+		)'
 end
 
 get '/' do
@@ -15,22 +39,59 @@ end
 
 get '/about' do
 	erb :about
-  end
+end
 
-  get '/visit' do
+get '/visit' do
 	erb :visit
-  end  
+end  
 
-  get '/contacts' do
-	erb :contacts
-  end
-
-  post '/visit' do
+post '/visit' do
 	@username = params[:username]
 	@phone = params[:phone]
 	@datetime = params[:datetime]
 	@barber = params[:barber]
 	@color = params[:colorpicker]
+
+	hh = {  :username => 'Insert your name',
+			:phone => 'Insert phone number',
+			:datetime => 'Insert date and time'}
+
+	@error = hh.select {|key,_| params[key] == ''}.values.join(", ")
+	if @error !=''
+		return erb :visit
+	end
 	
-	erb "You has beem enrolled, please check your data: #{@username}, #{@phone}, #{@datetime}, #{@barber}, #{@color}"
-  end  
+	db = get_db
+	db.execute 'insert into Clients
+	(username, phone, datestamp, barber, color)
+	values(?, ?, ?, ?, ?)',
+	[@username, @phone, @datetime, @barber, @color]
+
+	erb "You has been enrolled, please check your data: #{@username}, #{@phone}, #{@datetime}, #{@barber}, #{@color}"
+end  
+
+def get_db
+	return SQLite3::Database.new 'barbershop.db'
+end
+
+get '/contacts' do
+	erb :contactss
+end
+
+post '/contacts' do
+	@username = params[:username]
+	@usermessage = params[:usermessage]
+	
+	erb "Your message has been accepted, we'll call you back soon..."
+end  
+
+get '/showusers' do
+	db = get_db
+	db.execute 'select * from Clients' do |row|
+		puts row
+		puts ---------
+	
+  end
+  
+
+  
