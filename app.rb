7680c7c015ab2520e7f42659cbbb825a1a950ad4
Gedr <gimaev.ufa@gmail.com>
2022-08-28 10:46:19 +0900

    create barbershop.db

--- app.rb
+++ app.rb
@@ -2,10 +2,17 @@
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
+require 'sqlite3'
+
+configure do 
+	@db = SQLite3::Database.new 'barbershop.db'
+	
+end
 
 get '/' do
 	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
 end
+
 get '/about' do
 	erb :about
   end
@@ -23,8 +30,7 @@ get '/about' do
 	@phone = params[:phone]
 	@datetime = params[:datetime]
 	@barber = params[:barber]
+	@color = params[:colorpicker]
 	
-	erb "You has beem enrolled, please check your data: #{@username},#{@phone}, #{@datetime},#{@barber}"
+	erb "You has beem enrolled, please check your data: #{@username}, #{@phone}, #{@datetime}, #{@barber}, #{@color}"
   end  
-
-  
